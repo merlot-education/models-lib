8191c821ba1f90b1505936c6ce4df4e3a47be145
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-03-15 17:33:55 +0100

    add signer config

--- src/main/java/eu/merloteducation/modelslib/api/organization/MerlotParticipantMetaDto.java
+++ src/main/java/eu/merloteducation/modelslib/api/organization/MerlotParticipantMetaDto.java
@@ -9,9 +9,10 @@ import java.util.Set;
 @Getter
 @Setter
 public class MerlotParticipantMetaDto {
-    private String orgaId; // TODO the dto probably should not contain this...
+    private String orgaId;
     private String mailAddress;
     private MembershipClass membershipClass;
     private boolean active;
     private Set<OrganizationConnectorDto> connectors = new HashSet<>();
+    private OrganisationSignerConfigDto organisationSignerConfigDto;
 }
---
+++ src/main/java/eu/merloteducation/modelslib/api/organization/OrganisationSignerConfigDto.java
@@ -0,0 +1,14 @@
+package eu.merloteducation.modelslib.api.organization;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrganisationSignerConfigDto {
+    private String privateKey;
+
+    private String verificationMethod;
+}
