e00b0f7f88fd8881999890d558e3e617299a1464
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-04-08 15:21:01 +0200

    add models for daps certificates

---
+++ src/main/java/eu/merloteducation/modelslib/daps/OmejdnConnectorCertificateDto.java
@@ -0,0 +1,20 @@
+package eu.merloteducation.modelslib.daps;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OmejdnConnectorCertificateDto {
+    @JsonProperty("client_name")
+    private String clientName;
+    @JsonProperty("client_id")
+    private String clientId;
+    @JsonProperty("keystore")
+    private String keystore;
+    @JsonProperty("password")
+    private String password;
+}
